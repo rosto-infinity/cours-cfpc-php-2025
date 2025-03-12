3b3971a8b404e2a60e2ed53ad0728feb9d4d2e16
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-12 13:54:20 +0100

    // 3. Opérateurs d'assignation arithmétique

--- lecon-01/section-02-numbers.php
+++ lecon-01/section-02-numbers.php
@@ -1,15 +1,28 @@
 <?php
 // 1. Déclaration de nombres
-$a = 5;
+$a = -5;
 echo $a;
 $b = 4;
 $c = 1.2;
 echo "Addition" .($a + $b) . "<br>";
 
+$d=0;
 echo "Soustraction" .($a - $b) . "<br>";
 echo "Multiplication" .($a * $b) . "<br>";
 echo "Division" .($a / $c) . "<br>";
 echo "Modulo" .($a % $b) . "<br>";
 
+
 // Nouveauté PHP 8.x : fdiv() permet de diviser en gérant correctement les cas de division par zéro
-echo "Division flottante avec fdiv : " . fdiv($a, $c) . '<br>';
+echo "Division flottante avec fdiv : " . fdiv($a, $d) . '<br>';
+// 3. Opérateurs d'assignation arithmétique
+$a += $b; // équivaut à $a = $a + $b;
+echo "Après addition, $a = $a<br>";
+$a -= $b; // équivaut à $a = $a - $b;
+echo "Après soustraction, \$a = $a<br>";
+$a *= $b; // équivaut à $a = $a * $b;
+echo "Après multiplication, \$a = $a<br>";
+$a /= $b; // équivaut à $a = $a / $b;
+echo "Après division, \$a = $a<br>";
+$a %= $b; // équivaut à $a = $a % $b;
+echo "Après modulo, \$a = $a<br>";
