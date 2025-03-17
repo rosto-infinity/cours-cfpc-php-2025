00eb11d0607b5ecf2cd6235600db5f66e1ab7ca1
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-17 14:32:10 +0100

    Ajout de la validation du mot de passe et amélioration de la sécurité des entrées utilisateur

--- lecon-02/gestion-form.php
+++ lecon-02/gestion-form.php
@@ -1,19 +1,43 @@
 <?php
 if (isset($_POST['create'])) {
   //echo "Formulaire soumis";
-  
- // $prenom_student = $_POST['prenom'];
-  // $mail_student = $_POST['mail'];
+
+
+  //  // $prenom_student = $_POST['prenom'];
+//   // $mail_student = $_POST['mail'];
   if (empty($_POST['nom'])) {
     echo "Le nom est obligatoire <br>";
   } else {
-    $nom_student = $_POST['nom'];
+    $nom_student = htmlspecialchars($_POST['nom']) ;
 
-    echo "Nom: $nom_student <br>";
+      echo "Nom: $nom_student <br>";
   } 
-  echo "Prénom:  $prenom_student<br>";
- echo "Email:   $mail_studen <br>";
- 
+  if (empty($_POST['password'])) {
+    echo "Le password est obligatoire <br>";
+  } else {
+    $password_student = htmlspecialchars($_POST['password']) ;
+    $motDePasseHash = password_hash($password_student, PASSWORD_DEFAULT);
+    
+      echo "password: $motDePasseHash <br>";
+  } 
+//   if (empty($_POST['prenom'])) {
+//     echo "Le prenom est obligatoire <br>";
+//   } else {
+//     $prenom = $_POST['prenom'];
+
+  //     echo "Nom: $prenom <br>";
+//   } 
+
+  //   echo "Prénom:  $prenom_student<br>";
+//  echo "Email:   $mail_student <br>";
+
+  // $mail_student  = $_POST['mail'];
+  // if (!filter_var($mail_student, FILTER_VALIDATE_EMAIL)) {
+  //   echo "veuiller saisir un mail valide";
+  // }else{
+  //   echo "Email:   $mail_student <br>";
+  // }
+
 }
 
 ?>
@@ -37,13 +61,19 @@ if (isset($_POST['create'])) {
         class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500">
     </div>
     <div class="mb-4">
-      <input type="email" name="mail" placeholder="Email"
+      <input type="text" name="mail" placeholder="Email"
+        class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500">
+    </div>
+    <div class="mb-4">
+      <input type="password" name="password" placeholder="password"
         class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500">
     </div>
     <!-- texte centre -->
     <div class="text-center">
       <input type="submit" name="create" value="Créer"
         class="inline-block px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+      <button href="http: //localhost/php-2025/cours-php/cours-cfpc-php-2025/lecon-02/gestion-form.php"
+        class="inline-block px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"> Refresh</button>
     </div>
   </form>
 
