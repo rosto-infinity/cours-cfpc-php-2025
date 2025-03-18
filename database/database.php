fb91bae3ee6e5f0ac6d13924907b627e04ee0b1e
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-18 14:08:02 +0100

    index_student

--- database/database.php
+++ database/database.php
@@ -2,13 +2,24 @@
 $hostname="127.0.0.1";
 $username ="root";
 $password="";
-$database="cours-cfpc-php-2025l";
+$database="cours-cfpc-php-2025";
 
 $connect= new mysqli($hostname,$username, $password, $database);
 
-if ($connect->connect_error) {
-  echo "Erreur : La connexion à la base de données a échoué :  .$connect->connect_error "
-  ;
-} else {
-  echo "Succès : Connexion à la base de données avec succès !";
+// if ($connect->connect_error) {
+//   echo "Erreur : La connexion à la base de données a échoué :  .$connect->connect_error "
+//   ;
+// } else {
+//   echo "Succès : Connexion à la base de données avec succès !";
+// }
+try {
+  $conn = new mysqli($hostname, $username, $password, $database);
+    
+  if ($connect->connect_error) {
+      throw new Exception("La connexion à la base de données a échoué : " . $connect->connect_error);
+  }
+
+  echo "Succès : Connexion à la base de données établie avec succès !";
+} catch (Exception $e) {
+  echo "Erreur : " . $e->getMessage();
 }
---
+++ index_student.php
@@ -0,0 +1,20 @@
+<?php
+require_once "database/database.php";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>index_student</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
