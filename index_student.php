0cc50d7edd4e88bb375ebdc3e03c07be5422e387
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-18 14:44:20 +0100

    Ajout de la récupération et de l'affichage des données des étudiants dans index_student.php

--- index_student.php
+++ index_student.php
@@ -1,6 +1,31 @@
 <?php
 require_once "database/database.php";
 
+$sql="SELECT *FROM student";
+$result=$connect->query($sql);
+
+echo "<pre>";
+var_dump($result);
+echo "</pre>";
+if($result->num_rows >0)
+{
+foreach($result as $row)
+{
+
+  echo "</br>";
+  echo "Id : " .$row['id'].  "</br>";
+  echo "Nom : " .$row['nom'].  "</br>";
+  echo "Prenom : " .$row['prenom'].  "</br>";
+  echo "Mail : " .$row['mail'].  "</br>";
+  echo "Password : " .$row['password'].  "</br>";
+}
+}else{
+  echo "Aucun résultat trouvé";
+}
+
+
+
+
 
 
 ?>
