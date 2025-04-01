b98080428c1c2fb9fdb83accd19a0cb6e5b7895c
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-01 21:09:00 +0100

    02-Détruit la session

--- tp-02-espace-membre-2025/deconnexion.php
+++ tp-02-espace-membre-2025/deconnexion.php
@@ -2,5 +2,5 @@
 session_start();
 
 $_SESSION =[]; // 01-Détruit toutes les variables de session
-session_destroy(); // Détruit la session
+session_destroy(); // 02-Détruit la session
 header('Location: connexion.php');
