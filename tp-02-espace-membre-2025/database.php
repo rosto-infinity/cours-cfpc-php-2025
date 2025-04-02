8d3fb0c71fa7c3b7f6caa27ef03286da03d0cd99
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-02 19:11:19 +0100

    dsfsd

--- tp-02-espace-membre-2025/database.php
+++ tp-02-espace-membre-2025/database.php
@@ -3,6 +3,7 @@ $dsn = 'mysql:host=127.0.0.1;dbname=tp-02-espace-membre-2025';
 $username = 'root';//username 
 $password = ''; //password
 $options = [];
+
 try {
   $pdo= new PDO($dsn, $username, $password, $options);
   //options
